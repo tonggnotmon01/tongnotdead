4753d988d51e200a3ae63dcab79afee4a7e8f067
LawAng <tong.not.dead@gmail.com>
2019-02-26 14:08:03 +0700

    newup

---
+++ app/src/main/java/kritchanan/wongpadee/th/ac/bsru/MainActivity.java
@@ -0,0 +1,18 @@
+package kritchanan.wongpadee.th.ac.bsru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  Add Fragment to Activity
+        getSupportFragmentManager().beginTransaction().add(R.id.layoutMainFragmant, new MainFragment()).commit();
+
+
+    }   //Main Method
+}   //Main Class
